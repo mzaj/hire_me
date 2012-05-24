4fd97d28befcee84d93fa0c638d25018f0d6295d
Michał Zając <michal@michalzajac.eu>
2012-05-24 19:33:51 +0200

    Implement session actions

--- app/models/session.rb
+++ app/models/session.rb
@@ -2,7 +2,6 @@ module HireMe
   module Models
     class Session
       attr_accessor :user_first_name, :user_last_name, :questions
-    end
 
     def to_hash
       hash = {
@@ -10,8 +9,12 @@ module HireMe
         :user_last_name => user_last_name,
         :questions => []
       }
+
       questions.each { |q| hash[:questions].add(q.to_hash) }
       hash
+    end
+
+    def add_answer(question_id, answer)
     end
   end
 end
