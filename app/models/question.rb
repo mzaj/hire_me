44343c01dc0352c2814aa7167900769f22e70b4d
SirMike <mail@sirmike.org>
2012-05-24 19:25:32 +0200

    Add to_hash methods to Session and Question models

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,13 @@ module HireMe
   module Models
     class Question
       attr_accessor :id, :text
+
+      def to_hash
+        {
+          :id => id,
+          :text => text
+        }
+      end
     end
   end
 end
--- app/models/session.rb
+++ app/models/session.rb
@@ -2,9 +2,16 @@ module HireMe
   module Models
     class Session
       attr_accessor :user_first_name, :user_last_name, :questions
+    end
 
-      def to_hash
-      end
+    def to_hash
+      hash = {
+        :user_first_name => user_first_name,
+        :user_last_name => user_last_name,
+        :questions => []
+      }
+      questions.each { |q| hash[:questions].add(q.to_hash) }
+      hash
     end
   end
 end
