e672c344fa444eb5ac4d19dcf2b2de487992df48
Michał Zając <michal@michalzajac.eu>
2012-05-24 21:05:16 +0200

    Full Question serialization

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,19 +1,27 @@
 module HireMe
   module Models
     class Question
-      attr_accessor :id, :text
+      attr_accessor :id, :text, :type, :answers
+
+      def initialize
+        @answers = []
+      end
 
       def self.from_hash(question_hash)
         Question.new.tap do |question|
           question.id = question_hash['_id'].to_s
           question.text = question_hash['text']
+          question.type = question_hash['type']
+          question_hash['answers'].each { |a| question.answers << a }
         end
       end
 
       def to_hash
         {
           :_id => id,
-          :text => text
+          :text => text,
+          :type => type,
+          :answers => answers
         }
       end
 
