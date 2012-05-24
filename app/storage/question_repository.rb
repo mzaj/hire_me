608b12e10aa2865bff85b2faa3d6a8fb6826c261
Michał Zając <michal@michalzajac.eu>
2012-05-24 20:45:13 +0200

    Implement get_question method

--- app/storage/question_repository.rb
+++ app/storage/question_repository.rb
@@ -7,6 +7,10 @@ module HireMe
         collection.find.collect { |q| HireMe::Models::Question.from_hash(q) }
       end
 
+      def get_question(id)
+        question_hash = collection.find_one(BSON::ObjectId.from_string(id)).to_hash
+        HireMe::Models::Question.from_hash(question_hash)
+      end
       private
 
       def collection
