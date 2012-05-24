4b11c936de15bf68a07a273a3866559e88f96717
Michał Zając <michal@michalzajac.eu>
2012-05-24 17:28:49 +0200

    Split app and boot initializers

---
+++ app/app.rb
@@ -0,0 +1,10 @@
+HM_ROOT = File.expand_path(File.dirname(__FILE__) + '/..') unless defined?(HM_ROOT)
+HM_ENV = ENV["HM_ENV"] ||= ENV["RACK_ENV"] ||= "development" unless defined?(HM_ENV)
+
+require 'rubygems'
+require 'bundler/setup'
+Bundler.require(:default, HM_ENV)
+
+
+
+Dir["#{HM_ROOT}/app/models/**/*.rb"].each { |f| require(f) }
--- app/boot.rb
+++ app/boot.rb
@@ -1,13 +1,5 @@
-HM_ROOT = File.expand_path(File.dirname(__FILE__) + '/..') unless defined?(HM_ROOT)
-HM_ENV = ENV["HM_ENV"] ||= ENV["RACK_ENV"] ||= "development" unless defined?(HM_ENV)
+require 'app/app'
 
-require 'rubygems'
-require 'bundler/setup'
-Bundler.require(:default, HM_ENV)
-
-
-
-Dir["#{HM_ROOT}/app/models/**/*.rb"].each { |f| require(f) }
 Dir["#{HM_ROOT}/controllers/**/*.rb"].each { |f| require(f) }
 
 
