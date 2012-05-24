3f5b8984e67b7e3b450fc5e84d79d647ffc5eaca
Michał Zając <michal@michalzajac.eu>
2012-05-24 16:20:46 +0200

    Create QuestionService stub

---
+++ app/models/question_service.rb
@@ -0,0 +1,17 @@
+module HireMe
+  module Models
+    class QuestionService
+      def get_first_question
+      end
+
+      def get_next_question
+      end
+
+      def save_answer(question_number, answers)
+      end
+
+      def calculate_result
+      end
+    end
+  end
+end
