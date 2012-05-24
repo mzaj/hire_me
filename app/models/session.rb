0d66afec39c0cb5eff6ba500fcecf3ecdd652431
SirMike <mail@sirmike.org>
2012-05-24 19:14:26 +0200

    Add session model and create session

---
+++ app/models/session.rb
@@ -0,0 +1,11 @@
+module HireMe
+  module Models
+    class Session
+      attr_accessor :user_first_name, :user_last_name, :questions
+
+      def to_hash
+      end
+    end
+  end
+end
+
--- app/storage/session_repository.rb
+++ app/storage/session_repository.rb
@@ -6,6 +6,16 @@ module HireMe
       def collection
         @collection ||= get_collection('sessions')
       end
+
+      def create_session(user, question_list)
+        session = Session.new
+        session.user_first_name = user[:first_name]
+        session.user_last_name = user[:last_name]
+        session.questions = []
+
+        sessions = collection['sessions']
+        sessions.save(session.to_hash)
+      end
     end
   end
 end
